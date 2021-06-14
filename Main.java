ab5b728871009992e6ff99c66616125f86061fec
kiranreddy31 <85878091+kiranreddy31@users.noreply.github.com>
2021-06-14 19:37:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner i=new Scanner(System.in);
+		int k,y;
+		k=i.nextInt();
+                                   y=i.nextInt();
+		System.out.println(k+y);
+	}
+}
