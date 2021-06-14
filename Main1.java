ddf69f052228d64f72d754401dc6567ff00f7a20
kiranreddy31 <85878091+kiranreddy31@users.noreply.github.com>
2021-06-14 21:29:59 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main1
+{
+	public static void main(String[] args) {
+		Scanner i=new Scanner(System.in);
+		int k,j;
+		k=i.nextInt();
+		j=i.nextInt();
+		System.out.println("addition: "+(k+j));
+		System.out.println("subtarct:"+(k-j));
+       	System.out.println("multiplication: "+(k*j));
+		System.out.println("division: "+(j/k));
+				
+		
+	}
+}
