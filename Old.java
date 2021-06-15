f840df7b854642cfd2281df3a56ec44fa6b407dc
kiranreddy31 <85878091+kiranreddy31@users.noreply.github.com>
2021-06-15 17:21:52 +0530

    Add files via upload

---
+++ Old.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Odd{
+     public static void main (String[] args) {
+         int i=1;
+         System.out.println("printing odd numbers");
+         do{
+             if(i%2!=0)
+             {
+                System.out.println(i);
+             }
+             i++;
+         }while(i<=50);
+        
+       
+    }
+}
