95b1aeaf673790262246b2cac450e0d11205b4ee
kiranreddy31 <85878091+kiranreddy31@users.noreply.github.com>
2021-06-16 18:00:51 +0530

    Add files via upload

---
+++ Pi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+class Pi {
+    public void getPi()
+    {
+        double x = Math.PI;
+     System.out.println(x);
+    }
+  public static void main(String[] args) {
+     Pi o=new Pi();
+     o.getPi();
+  }
+}
